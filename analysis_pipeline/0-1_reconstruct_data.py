3e577f5d29c164b5865077d13101c5b7319ac354
Harry Staley <staleyh@gmail.com>
2026-06-25 19:52:28 -0400

    pulled updated data from qualtrics.

--- analysis_pipeline/0-1_reconstruct_data.py
+++ analysis_pipeline/0-1_reconstruct_data.py
@@ -59,7 +59,7 @@ import tempfile
 # EXPORT may be EITHER a Qualtrics .zip export OR the .csv inside it. If a .zip
 # is given, the CSV is extracted automatically (you do not need to unzip first).
 EXPORT_DIR = "../qdata"
-EXPORT_FILE = "AI_Attestation_Trust_Study_06-24-2026T0635.zip"
+EXPORT_FILE = "AI_Attestation_Trust_Study_06-25-2026T1938.zip"
 VERSION_DIR = "../versions_output"   # folder holding version_1.csv .. version_6.csv
 OUTPUT_DIR = "../prepped_data"
 OUTPUT_CSV = "long_reconstructed.csv"
